6e5d9a27863e03273df8a1c9802f8705f2e3c68e
NightHawk-Michael <michael.le611@gmail.com>
2018-03-08 11:51:01 +0800

    modularize training algorithm

--- distributed-deep-mnist.py
+++ distributed-deep-mnist.py
@@ -4,6 +4,7 @@ import os
 import stat
 import logging
 import zipfile
+import trainingalgorithm
 
 from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
@@ -15,50 +16,50 @@ from tensorflow.python.util import compat
 
 FLAGS = None
 
-def deepnn(x):
-  x_image = tf.reshape(x, [-1, 28, 28, 1])
+# def deepnn(x):
+#   x_image = tf.reshape(x, [-1, 28, 28, 1])
 
-  W_conv1 = weight_variable([5, 5, 1, 32])
-  b_conv1 = bias_variable([32])
-  h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+#   W_conv1 = weight_variable([5, 5, 1, 32])
+#   b_conv1 = bias_variable([32])
+#   h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 
-  h_pool1 = max_pool_2x2(h_conv1)
+#   h_pool1 = max_pool_2x2(h_conv1)
 
-  W_conv2 = weight_variable([5, 5, 32, 64])
-  b_conv2 = bias_variable([64])
-  h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+#   W_conv2 = weight_variable([5, 5, 32, 64])
+#   b_conv2 = bias_variable([64])
+#   h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 
-  h_pool2 = max_pool_2x2(h_conv2)
+#   h_pool2 = max_pool_2x2(h_conv2)
 
-  W_fc1 = weight_variable([7 * 7 * 64, 1024])
-  b_fc1 = bias_variable([1024])
+#   W_fc1 = weight_variable([7 * 7 * 64, 1024])
+#   b_fc1 = bias_variable([1024])
 
-  h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
-  h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+#   h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+#   h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
-  keep_prob = tf.placeholder(tf.float32)
-  h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+#   keep_prob = tf.placeholder(tf.float32)
+#   h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
-  W_fc2 = weight_variable([1024, 10])
-  b_fc2 = bias_variable([10])
+#   W_fc2 = weight_variable([1024, 10])
+#   b_fc2 = bias_variable([10])
 
-  y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
-  return y_conv, keep_prob
+#   y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
+#   return y_conv, keep_prob
 
-def conv2d(x, W):
-  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+# def conv2d(x, W):
+#   return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
-def max_pool_2x2(x):
-  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                        strides=[1, 2, 2, 1], padding='SAME')
+# def max_pool_2x2(x):
+#   return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+#                         strides=[1, 2, 2, 1], padding='SAME')
 
-def weight_variable(shape):
-  initial = tf.truncated_normal(shape, stddev=0.1)
-  return tf.Variable(initial)
+# def weight_variable(shape):
+#   initial = tf.truncated_normal(shape, stddev=0.1)
+#   return tf.Variable(initial)
 
-def bias_variable(shape):
-  initial = tf.constant(0.1, shape=shape)
-  return tf.Variable(initial)
+# def bias_variable(shape):
+#   initial = tf.constant(0.1, shape=shape)
+#   return tf.Variable(initial)
 
 def get_session(sess):
   session = sess
@@ -134,7 +135,7 @@ def main(_):
         cluster=cluster)):
 
       # Import data
-      mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+      dataset = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
 
       # Build Deep MNIST model...
       keys_placeholder = tf.placeholder(tf.int32, shape=[None, 1])
@@ -143,7 +144,7 @@ def main(_):
       # TODO: Change this 3 lines for new model implementation
       x = tf.placeholder(tf.float32, [None, 784])
       y_ = tf.placeholder(tf.float32, [None, 10])
-      y_conv, keep_prob = deepnn(x)
+      y_conv, keep_prob = trainingalgorithm.deepnn(x)
 
       cross_entropy = tf.reduce_mean(
           tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
@@ -184,7 +185,7 @@ def main(_):
       i = 0
       while not mon_sess.should_stop():
         # Run a training step asynchronously.
-        batch = mnist.train.next_batch(50)
+        batch = dataset.train.next_batch(50)
         if i % 100 == 0:
           train_accuracy = mon_sess.run(accuracy, feed_dict={
               x: batch[0], y_: batch[1], keep_prob: 1.0})
