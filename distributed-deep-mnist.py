6355feb38ebab8614742b3ba64cd2f71f5f9d2ee
NightHawk-Michael <michael.le611@gmail.com>
2018-03-27 00:57:32 +0800

    fix saver issues

--- distributed-deep-mnist.py
+++ distributed-deep-mnist.py
@@ -131,6 +131,7 @@ def main(_):
         y_conv, keep_prob = trainingalgorithm.trainingAlgorithm(x)
         y_conv = tf.identity(y_conv, name='y_conv')
 
+
         cross_entropy = tf.reduce_mean(
             tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y_conv))
 
