2df775831259f57e46ef81e878afda7da0e55794
Ubuntu <ubuntu@ip-172-31-13-28.ap-southeast-1.compute.internal>
2018-03-30 18:44:23 +0000

    handle file in post request

--- serving/myproject.py
+++ serving/myproject.py
@@ -21,79 +21,78 @@ app = Flask(__name__)
 
 @app.route("/", methods = ['GET','POST'])
 def index():
-	#host, port, image = parse_args()
-	try:
-		host = "localhost"
-		port = 9000
-		channel = implementations.insecure_channel(host, int(port))
-		stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
+    #host, port, image = parse_args()
+    try:
+    	host = "localhost"
+    	port = 9000
+    	channel = implementations.insecure_channel(host, int(port))
+    	stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
 
-		# dataset = input_data.read_data_sets('/tmp/Mnist', one_hot=True)
-		# batch = dataset.train.next_batch(1)
-		# print(batch[1])
-		if(request.method == 'GET'):
-			img = [[0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
-			numpyarray = np.array(img, np.float32)
-			print("Expected result: %d" % (2))
-		if(request.method == 'POST'):
-			nparr = np.fromstring(request.data, np.unit8)
-			imgdecoded = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-			print(imgdecoded)
-			#imgurl = request.form['url']
-			# os.system("wget -O /home/ubuntu/serveimg/img.jpg " + imgurl)
-			#subprocess.Popen(["wget","-O", "/home/ubuntu/serveimg/img.jpg",imgurl], stdout=subprocess.PIPE)
-			#img = cv2.imread('/home/ubuntu/serveimg/img.jpg')
-			#imgarray = []
-			# for i in range(0, len(img)):
-			#     for j in range(0, len(img[i])):
-			#         tmp = img[i][j]
-			#         px = 0
-			#         for item in tmp:
-			#             if(item > 5):
-			#                 px = 1
-			#                 break
-			#         imgarray.append(px)
-			# numpyarray = np.array(imgarray, np.float32)
-			return "test"
+    	# dataset = input_data.read_data_sets('/tmp/Mnist', one_hot=True)
+    	# batch = dataset.train.next_batch(1)
+    	# print(batch[1])
+    	if(request.method == 'GET'):
+            img = [[0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+            numpyarray = np.array(img, np.float32)
+	    print("Expected result: %d" % (2))
+	if(request.method == 'POST'):
+            file = request.files['file']
+            if file:
+                file.save(os.path.join('/home/ubuntu/serveimg/','img.jpg'))
+		#imgurl = request.form['url']
+	    	# os.system("wget -O /home/ubuntu/serveimg/img.jpg " + imgurl)
+    		#subprocess.Popen(["wget","-O", "/home/ubuntu/serveimg/img.jpg",imgurl], stdout=subprocess.PIPE)
+    		img = cv2.imread('/home/ubuntu/serveimg/img.jpg')
+    		imgarray = []
+    		for i in range(0, len(img)):
+    		    for j in range(0, len(img[i])):
+    		        tmp = img[i][j]
+    		        px = 0
+    		        for item in tmp:
+    		            if(item > 5):
+    	                        px = 1
+    		                break
+    		        imgarray.append(px)
+    		numpyarray = np.array(imgarray, np.float32)
 
-		start = time.time()
+        start = time.time()
 
-		servingrequest = predict_pb2.PredictRequest()
+	servingrequest = predict_pb2.PredictRequest()
 		
-		servingrequest.model_spec.name = 'mnist'
+	servingrequest.model_spec.name = 'mnist'
 	    
-		servingrequest.model_spec.signature_name = 'predict_images'
+	servingrequest.model_spec.signature_name = 'predict_images'
 
-		servingrequest.inputs['images'].CopyFrom(make_tensor_proto(numpyarray, shape=[1, 784]))
+	servingrequest.inputs['images'].CopyFrom(make_tensor_proto(numpyarray, shape=[1, 784]))
 		
-		result = stub.Predict(servingrequest, 60.0)  # 60 secs timeout
+	result = stub.Predict(servingrequest, 60.0)  # 60 secs timeout
 
 	    
-		end = time.time()
+	end = time.time()
 	    
-		time_diff = end - start
+	time_diff = end - start
 
-		print(result.outputs['scores'].float_val)
-		print(result)
+	print(result.outputs['scores'].float_val)
+	print(result)
 	    
-		print('time elapased: {}'.format(time_diff))
+	print('time elapased: {}'.format(time_diff))
 
-		result_list = result.outputs['scores'].float_val
+	result_list = result.outputs['scores'].float_val
 
-		max_val = max(result_list)
+	max_val = max(result_list)
 
-		num_result = 0
-		for i in range(0,len(result_list)):
-			if(result_list[i] == max_val):
-				num_result = i
+	num_result = 0
+	for i in range(0,len(result_list)):
+	    if(result_list[i] == max_val):
+                num_result = i
 		
-		return str(num_result)
-	except Exception as e:
-		with open('/home/ubuntu/myproject/log.txt', 'a+') as f:
-			error = "\n\n Internal Server Error \n" + str(e)
-			f.write(error)
-			f.close()
+	return str(num_result)
+    except Exception as e:
+    	with open('/home/ubuntu/myproject/log.txt', 'a+') as f:
+	    error = "\n\n Internal Server Error \n" + str(e)
+	    f.write(error)
+	    f.close()
 
 if __name__ == "__main__":
-	app.debug = False
+    app.debug = False
     app.run(host='0.0.0.0')
