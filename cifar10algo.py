88d8382d5abe2ecb6ffe42630280e875a099469a
NightHawk-Michael <michael.le611@gmail.com>
2018-05-12 03:16:44 +0800

    add cifar10 codes

---
+++ cifar10algo.py
@@ -0,0 +1,84 @@
+import prettytensor as pt
+def pre_process_image(image, training):
+    # This function takes a single image as input,
+    # and a boolean whether to build the training or testing graph.
+    
+    if training:
+        # For training, add the following to the TensorFlow graph.
+
+        # Randomly crop the input image.
+        image = tf.random_crop(image, size=[img_size_cropped, img_size_cropped, num_channels])
+
+        # Randomly flip the image horizontally.
+        image = tf.image.random_flip_left_right(image)
+        
+        # Randomly adjust hue, contrast and saturation.
+        image = tf.image.random_hue(image, max_delta=0.05)
+        image = tf.image.random_contrast(image, lower=0.3, upper=1.0)
+        image = tf.image.random_brightness(image, max_delta=0.2)
+        image = tf.image.random_saturation(image, lower=0.0, upper=2.0)
+
+        # Some of these functions may overflow and result in pixel
+        # values beyond the [0, 1] range. It is unclear from the
+        # documentation of TensorFlow 0.10.0rc0 whether this is
+        # intended. A simple solution is to limit the range.
+
+        # Limit the image pixels between [0, 1] in case of overflow.
+        image = tf.minimum(image, 1.0)
+        image = tf.maximum(image, 0.0)
+    else:
+        # For training, add the following to the TensorFlow graph.
+
+        # Crop the input image around the centre so it is the same
+        # size as images that are randomly cropped during training.
+        image = tf.image.resize_image_with_crop_or_pad(image,
+                                                       target_height=img_size_cropped,
+                                                       target_width=img_size_cropped)
+
+    return image
+def pre_process(images, training):
+    # Use TensorFlow to loop over all the input images and call
+    # the function above which takes a single image as input.
+    images = tf.map_fn(lambda image: pre_process_image(image, training), images)
+
+    return images
+def main_network(images, training):
+    # Wrap the input images as a Pretty Tensor object.
+    x_pretty = pt.wrap(images)
+
+    # Pretty Tensor uses special numbers to distinguish between
+    # the training and testing phases.
+    if training:
+        phase = pt.Phase.train
+    else:
+        phase = pt.Phase.infer
+
+    # Create the convolutional neural network using Pretty Tensor.
+    # It is very similar to the previous tutorials, except
+    # the use of so-called batch-normalization in the first layer.
+    with pt.defaults_scope(activation_fn=tf.nn.relu, phase=phase):
+        	y_pred, loss = x_pretty
+            conv2d(kernel=5, depth=64, name='layer_conv1', batch_normalize=True)
+            max_pool(kernel=2, stride=2)
+            conv2d(kernel=5, depth=64, name='layer_conv2')
+            max_pool(kernel=2, stride=2)
+            flatten()
+            fully_connected(size=256, name='layer_fc1')
+            fully_connected(size=128, name='layer_fc2')
+            softmax_classifier(num_classes=num_classes, labels=y_true)
+
+    return y_pred, loss
+def trainingAlgorithm(training):
+    # Wrap the neural network in the scope named 'network'.
+    # Create new variables during training, and re-use during testing.
+    with tf.variable_scope('network', reuse=not training):
+        # Just rename the input placeholder variable for convenience.
+        images = x
+
+        # Create TensorFlow graph for pre-processing.
+        images = pre_process(images=images, training=training)
+
+        # Create TensorFlow graph for the main processing.
+        y_pred, loss = main_network(images=images, training=training)
+
+    return y_pred
