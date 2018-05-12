79c8b71d2495b3934f3ee62eeed454578197a691
NightHawk-Michael <michael.le611@gmail.com>
2018-05-12 13:57:53 +0800

    debug cifar10

--- distributed-cifar10.py
+++ distributed-cifar10.py
@@ -205,22 +205,21 @@ def main(_):
         print("start session")
         i = 0
         cur_batch = 0
-        while not mon_sess.should_stop() and cur_batch < 100:
+        while not mon_sess.should_stop() and cur_batch < 1000:
           # Run a training step asynchronously.
           #batch = dataset.train.next_batch(50)
           cur_batch = 0
           print(len(train_x))
-          batch = []
-          batch[0] = train_x[cur_batch : cur_batch+50]
-          batch[1] = train_y[cur_batch : cur_batch+50]
+          batch_x = train_x[cur_batch : cur_batch+50]
+          batch_y = train_y[cur_batch : cur_batch+50]
           if i % 100 == 0:
             # train_accuracy = mon_sess.run(accuracy, feed_dict={
             #     x: batch[0], y_: batch[1], keep_prob: 0.9})
             train_accuracy = mon_sess.run(accuracy, feed_dict={
-                x: batch[0], y_: batch[1]})
+                x: batch_x, y_: batch_y})
             print('Global_step %s, task:%d_step %d, training accuracy %g' % (tf.train.global_step(mon_sess, global_step), FLAGS.task_index, i, train_accuracy))
           #mon_sess.run(train_step, feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-          mon_sess.run(train_step, feed_dict={x: batch[0], y_: batch[1]})
+          mon_sess.run(train_step, feed_dict={x: batch_x, y_: batch_y})
           print('Global_step %s, task:%d_step %d, training accuracy %g' % (tf.train.global_step(mon_sess, global_step), FLAGS.task_index, i, train_accuracy))
           i = i + 1
         #stop_time = time.time()
