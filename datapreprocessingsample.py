ea72af78c1784c6e3f89b096fc67ebe4e522fbae
NightHawk-Michael <michael.le611@gmail.com>
2018-05-13 18:41:28 +0800

    add code to run both mnist and cifar10

---
+++ datapreprocessingsample.py
@@ -0,0 +1,24 @@
+#MNIST
+from tensorflow.examples.tutorials.mnist import input_data
+dataset = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
+
+def getImageData():
+	imageData = dataset.train.images
+	return imageData
+
+def getLabelData():
+	labelData = dataset.train.labels
+	return labelData
+
+#CIFAR-10
+from additionalinfo import get_data_set
+train_x, train_y = get_data_set("train")
+test_x, test_y = get_data_set("test")
+
+def getImageData():
+	imageData = train_x
+	return imageData
+
+def getLabelData():
+	labelData = train_y
+	return labelData
